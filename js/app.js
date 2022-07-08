abfbde02df4785e2064f7334361a07b25cb9a57d
Dmitry Podlesnykh <qunaxis@gmail.com>
2022-07-09 02:39:51 +0300

    feat: initial

---
+++ js/app.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+	// Custom JS
+	const typed = new Typed('.cta__description_typer', {
+		strings: [
+			'Node.js',
+			'Typescript',
+			'Golang',
+			'Python',
+			'Storages and message brokers',
+			'Amazon Web Services',
+			'Docker',
+			'Kubernetes',
+			'Microservices',
+			'Event-driven architecture',
+			'Fault-tolerant design',
+		],
+		typeSpeed: 50,
+		backSpeed: 25,
+		// fadeOut: true,
+		loop: true
+	})
+}
