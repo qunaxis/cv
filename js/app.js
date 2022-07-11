e549738dd4a4575d433243c1e4e6d17f3aeaf75a
Dmitry Podlesnykh <qunaxis@gmail.com>
2022-07-11 18:33:31 +0300

    feat: typed changed to be seo friendly

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
 window.onload = function() {
 	// Custom JS
-	const typed = new Typed('.cta__description_typer', {
-		strings: [
-			'Node.js',
-			'Typescript',
-			'Golang',
-			'Python',
-			'Storages and msg brokers',
-			'Amazon Web Services',
-			'Docker',
-			'Kubernetes',
-			'Microservices',
-			'Event-driven architecture',
-			'Fault-tolerant design',
-		],
+	const typed = new Typed('.cta__description', {
+		stringsElement: '#typed-strings',
+		smartTyping: true,
 		typeSpeed: 50,
 		backSpeed: 25,
 		// fadeOut: true,
